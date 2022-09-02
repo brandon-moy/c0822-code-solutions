9f97bfa796bdb39d27d2ca47fa892df533a63453
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-02 03:10:11 +0000

    revisions made to compact function js file

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,7 +2,17 @@
 
 // Goal : take an array input and remove various values form the string
 // create a spot for the output
-// create a list of banned words
-// check if each location is equal to any part of the array?
+// create a list of banned values
+// check if each location is equal to any part of the banned values
 // if the values are not equal, add the value to the array
 // return the output
+
+function compact(array) {
+  var output = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== false && array[i] !== null && !Number.isNaN(array[i]) && array[i] !== 0 && array[i] !== undefined && array[i] !== '') {
+      output.push(array[i]);
+    }
+  }
+  return output;
+}
