205651e4783941a2384816945bdbfb3c3e31e41d
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-06 16:28:38 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var firstH1 = document.querySelector('h1');
+console.log('$heading', firstH1);
+console.dir(firstH1);
+
+var explanation = document.querySelector('#explanation');
+console.log('$explanation', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('$hint', hint);
+console.dir(hint);
+
+var nodeList = document.querySelectorAll('p');
+console.log('$paragraph', nodeList);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log('$links', exampleLink);
