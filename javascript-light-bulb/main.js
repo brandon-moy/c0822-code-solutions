8a58a265f4aee48522af6ff641e1909140ca6e13
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-06 23:28:01 +0000

    added light variable for condition instead of using DOM state

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,18 @@
 var $lightBulb = document.querySelector('.light-bulb');
 var $background = document.querySelector('.light');
+var light = 'on';
 
 function flipSwitch(event) {
   var background = '';
   var lightBulb = '';
-  if ($background.className === 'light') {
+  if (light === 'on') {
     background = 'dark';
     lightBulb = 'column-fifth lights-off';
+    light = 'off';
   } else {
     background = 'light';
     lightBulb = 'column-fifth light-bulb';
+    light = 'on';
   }
   $background.className = background;
   $lightBulb.className = lightBulb;
