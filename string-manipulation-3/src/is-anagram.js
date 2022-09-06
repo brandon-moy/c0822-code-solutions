ad24cb1cbf27eb1605ac4251f84d5f471833f96a
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-06 16:57:48 +0000

    passed isPalindromic, isAnagramic, and reverseWords, planning started for titleCase

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -5,13 +5,15 @@
 // create two new variables for the two strings
 // remove any spaces between the strings
 // sort the letters
-// combine the strings into one word and assign to new variable
+// combine the strings into one word
+// remove any excess spaces
+// assign to new variable
 // check if the two strings are equal to each other
 // if yes return true otherwise return false
 
 function isAnagram(firstString, secondString) {
-  var sortFirstString = firstString.split('').sort().join('');
-  var sortSecondString = secondString.split('').sort().join('');
+  var sortFirstString = firstString.split('').sort().join('').trim();
+  var sortSecondString = secondString.split('').sort().join('').trim();
   if (sortFirstString === sortSecondString) {
     return true;
   }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,16 @@
 /* exported titleCase */
+
+// Goal: take a string and return it with capitalization of title case
+// title case: capitalize 4 letters or more
+//             capitalize first word of title and any subtitle
+//             capitalize major words and second aprt of hyphenated words
+//             JavaScript is always JavaScript and API is always API (hard code)
+// takes a string
+// create an output for the new string
+// create a variable to hold a divided up string
+// lowercase the string
+// split the string and assign to the variable
+// for a loop
+// if === javascript - JavaScript
+// else if === api - API
+//
