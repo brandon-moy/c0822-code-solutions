1c93290358d6dc1f91346e88a7bf0f6bd23c718b
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-07 17:21:35 +0000

    started planning/writing title-case function

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -19,3 +19,6 @@ function isAnagram(firstString, secondString) {
   }
   return false;
 }
+
+// Take 2
+// take the strings
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -7,10 +7,21 @@
 //             JavaScript is always JavaScript and API is always API (hard code)
 // takes a string
 // create an output for the new string
-// create a variable to hold a divided up string
-// lowercase the string
-// split the string and assign to the variable
-// for a loop
-// if === javascript - JavaScript
-// else if === api - API
+// lowercase original string into a new variable
+// turn original string into an array without spaces
+// create an array of minor words
+// first word should always be capitalized and add to new output charAt(0).toUpperCase() + substring(1);
+// start to check each word starting with the second word
+// if javascript - JavaScript
+// if api - API
+// if includes a '-' give indexOf '-' and capitalize at position 0 and index + 1
+// if minor word skip
 //
+
+function titleCase(title) {
+  var output = '';
+  var lowercase = title.toLowerCase();
+  var lowercaseArray = lowercase.split(' ');
+  output = lowercaseArray[0].charAt(0).toUpperCase() + lowercaseArray[0].substring(1);
+  return output;
+}
