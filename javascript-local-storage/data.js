f0388350295844f54961605ed5571ebbe9c2616c
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-09 17:57:01 +0000

    moved beforeUnlaod function and eventListener to data.js file

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,3 +3,10 @@
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 todos = JSON.parse(previousTodosJSON);
+
+function beforeUnload(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
--- javascript-local-storage/main.js
+++ javascript-local-storage/main.js
@@ -71,10 +71,3 @@ function renderTodo(todo) {
 
   return $todo;
 }
-
-function beforeUnload(event) {
-  var todosJSON = JSON.stringify(todos);
-  localStorage.setItem('javascript-local-storage', todosJSON);
-}
-
-window.addEventListener('beforeunload', beforeUnload);
