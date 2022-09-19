db13ab8464861fcf7f3ccb169b1765a77acc02c8
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-19 16:25:57 +0000

    Completed javascript-custom-methods exercise.

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,40 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return (x + y);
+  },
+
+  subtract: function (x, y) {
+    return (x - y);
+  },
+
+  multiply: function (x, y) {
+    return (x * y);
+  },
+
+  divide: function (x, y) {
+    return (x / y);
+  },
+
+  square: function (x) {
+    return (x * x);
+  },
+
+  sumAll: function (numbers) {
+    var sumAll = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sumAll += numbers[i];
+    }
+    return sumAll;
+  },
+
+  getAverage: function (numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    var average = sum / numbers.length;
+    return average;
+  }
+};
