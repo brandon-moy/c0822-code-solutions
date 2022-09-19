e442f60732a7cb6ae2ef51d48790a966b9090491
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-19 21:11:54 +0000

    Completed javascript-constructors exercise.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var obj = new Object();
+var arr = new Array();
+var func = new Function();
+var string = new String();
+var num = new Number();
+var bool = new Boolean();
+
+console.log('value of obj:', obj);
+console.log('typeof obj:', typeof obj);
+console.log('value of arr:', arr);
+console.log('typeof arr:', typeof arr);
+console.log('value of func:', func);
+console.log('typeof func:', typeof func);
+console.log('value of num:', num);
+console.log('typeof num:', typeof num);
+console.log('value of bool:', bool);
+console.log('typeof bool:', typeof bool);
+console.log('value of string:', string);
+console.log('typeof string:', typeof string);
