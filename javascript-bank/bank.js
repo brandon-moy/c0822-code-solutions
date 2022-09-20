cd560953d2972fcf85e5e10696feb77d3e3afab9
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-20 03:08:53 +0000

    Planning functions for prototype openAccount and getAccount

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,33 @@
 /* exported Bank */
+
+function Bank() {
+  this.accounts = [];
+  this.nextAccountNumber = 1;
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var account = {
+      number: this.nextAccountNumber,
+      holder,
+      balance
+    };
+    this.nextAccountNumber++;
+    this.accounts.push(account);
+    return account.number;
+  }
+  return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+
+};
