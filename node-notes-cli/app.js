081e6e3b681fd29b952a88cc13eea6dc2073e1ca
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-12 23:23:47 +0000

    Created function to add entry and update data.json file

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,12 +1,12 @@
-// const fs = require('fs');
+const fs = require('fs');
 const data = require('./data.json');
 
 const option = process.argv[2];
 
-if (option === 'create') {
-  // createEntry(process.argv[3]);
-} else if (option === 'read') {
+if (option === 'read') {
   displayEntries();
+} else if (option === 'create') {
+  createEntry(process.argv[3]);
 }
 //  else if (option === 'update') {
 //   updateEntry(process.argv[3], process.argv[4]);
@@ -18,4 +18,18 @@ function displayEntries() {
   for (const key in data.notes) {
     console.log(key + ':', data.notes[key]);
   }
+}
+
+function createEntry(string) {
+  const id = data.nextId;
+  data.nextId++;
+  data.notes[id] = string;
+  const update = JSON.stringify(data, null, 2);
+  fs.writeFile('./data.json', update, err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+  console.log(data);
 }
