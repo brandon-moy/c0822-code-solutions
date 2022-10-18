acb03f7067f39477ebf41cfb583387d655c6b2ea
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-18 21:27:29 +0000

    Adjusting incorrect solution to profit-margin challenge - list incorrect - look at CTE

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,12 +1,14 @@
 select
        "films"."title",
-       "films"."description",
-       "films"."rating",
-       (sum("amount") - "films"."replacementCost")  as "totalProfit"
+      --  "films"."description",
+      --  "films"."rating",
+      -- "films"."filmId",
+      --  (sum("payments"."amount") - ("films"."replacementCost")) as "totalProfit",
+       "inventory"."filmId"
   from "payments"
   join "rentals" using ("rentalId")
   join "inventory" using ("inventoryId")
   join "films" using ("filmId")
   group by "filmId"
-  order by "paid" desc
-  limit 5;
+  -- order by "totalProfit" desc
+  limit 10;
