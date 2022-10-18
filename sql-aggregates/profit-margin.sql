fdadbc1093daa1d909e63ec4c897b9ecb00ffe16
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-18 22:37:20 +0000

    Used CTE to return correct totalProfit amounts in profit-margin.sql

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -14,26 +14,11 @@ select
        "title",
        "description",
        "rating",
-       "replacement",
       (sum("payments"."amount") - "replacement") as "totalProfit"
   from filmCount
   join "inventory" using ("filmId")
   join "rentals" using ("inventoryId")
   join "payments" using ("rentalId")
-  group by "title"
+  group by ("title", "description", "rating", "replacement")
   order by "totalProfit" desc
-  limit 10;
-
--- select
---        "films"."title",
---       --  "films"."description",
---       --  "films"."rating",
---       -- "films"."filmId",
---        (sum("payments"."amount") - ("films"."replacementCost" * price."count")) as "totalProfit"
---   from "payments"
---   join "rentals" using ("rentalId")
---   join "inventory" using ("inventoryId")
---   join "films" using ("filmId")
---   group by "filmId"
---   -- order by "totalProfit" desc
---   limit 10;
+  limit 5;
