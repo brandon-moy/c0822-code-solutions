91d6a3c2dd24dfa27498f87c319c5b207262df12
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-19 17:30:17 +0000

    Update option create and tests run to stop and return correct error messages and status codes

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -39,7 +39,7 @@ app.post('/api/grades', (req, res) => {
   const student = req.body;
   if (!student.course || !student.name || !student.score) {
     res.status(400).json({
-      error: 'Name, Course, and Score are required.'
+      error: '"name", "course", and "score" are required.'
     });
     return;
   }
@@ -60,12 +60,65 @@ app.post('/api/grades', (req, res) => {
   db.query(sql, values)
     .then(result => {
       res.status(201);
-      res.json(result.rows);
+      res.json(result.rows[0]);
     })
     .catch(err => {
       console.error(err);
       res.status(500).json({
         error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: '"gradeId" must be a positive integer'
+    });
+    return;
+  }
+  const student = req.body;
+  if (!student.course || !student.name || !student.score) {
+    res.status(400).json({
+      error: '"name", "course", and "score" are required.'
+    });
+    return;
+  }
+  const grade = Number(student.score);
+  if (!Number.isInteger(grade) || Number(grade < 0 || Number(grade > 100))) {
+    res.status(400).json({
+      error: 'Please input a positive number between 0 and 100.'
+    });
+    return;
+  }
+
+  const sql = `
+    update "grades"
+      set "name" = $1,
+          "course" = $2,
+          "score" = $3
+    where "gradeId" = $4
+    returning *
+  `;
+
+  const values = [student.name, student.course, student.score, gradeId];
+
+  db.query(sql, values)
+    .then(result => {
+      if (!result.rows[0]) {
+        res.status(404).json({
+          error: '"gradeId" not found. Please check the "gradeId" and try again'
+        });
+        return;
+      }
+      res.status(200);
+      res.json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured.'
       });
     });
 });
