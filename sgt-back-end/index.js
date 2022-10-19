126bb122325c89659a73eed2ed0ac8803575d27d
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-19 16:38:56 +0000

    Created get option to return all items from grades database

---
+++ sgt-back-end/index.js
@@ -0,0 +1,39 @@
+const pg = require('pg');
+const express = require('express');
+
+const app = express();
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+    select "gradeId",
+           "name",
+           "course",
+           "score",
+           "createdAt"
+      from "grades"
+    `;
+
+  db.query(sql)
+    .then(result => {
+      const grades = (result.rows);
+      res.json(grades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
