242c10f9f34cb927a8022b286dc8ea7a0280aeeb
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-25 17:48:16 +0000

    Completed react-hot-button-component challenge

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import Button from './hot-button';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<Button />);
