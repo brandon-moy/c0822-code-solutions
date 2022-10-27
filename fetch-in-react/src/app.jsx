82e82aceb630e5ebf309e6cc30ff981009c6491e
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-10-27 21:48:45 +0000

    Completed fetch-in-react exercise

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: false });
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => this.setState({ users: data }))
+      // eslint-disable-next-line no-console
+      .catch(err => console.err(err));
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
