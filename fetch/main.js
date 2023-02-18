015f849559554a10c034ee8575dc198558420cb7
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-02-18 22:38:31 +0000

    Created async functions to fetch data

--- fetch/main.js
+++ fetch/main.js
@@ -5,3 +5,30 @@ fetch('https://jsonplaceholder.typicode.com/users')
 fetch('https://pokeapi.co/api/v2/pokemon/wooper')
   .then(response => response.json())
   .then(data => console.log(data));
+
+async function getUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getPokemon() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/wooper');
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getUsers();
+getPokemon();
